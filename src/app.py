b2f2b2cc1f4d41feeb6215c99f7dea2e30830ed5
Kunal-Jain7 <130764854+Kunal-Jain7@users.noreply.github.com>
2025-12-04 23:10:31 +0530

    Updating the correct python code for testing

--- src/app.py
+++ src/app.py
@@ -1,24 +1,12 @@
-from flask import Flask, jsonify
-import datetime
-import socket
+from flask import Flask
+import os
 
 app = Flask(__name__)
 
-@app.route('/api/v1/details')
-
-def details():
-    return jsonify({
-        'time': datetime.datetime.now().strftime("%I:%M:%S%p on %B %d, %Y"),
-        'hostname': socket.gethostname(),
-        'message' 'This is Great..!! Keep it up !!'
-    })
-
-@app.route('/api/v1/healthz')
-
-def healthz():
-    return jsonify({
-        'status': 'up'
-    }), 200
+@app.route("/")
+def wish();
+    message = "Happy Birthday {name}"
+    return message.format(name=os.getenv("NAME", "Kunal"))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
